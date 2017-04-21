3ac5520cb8f3db55503db7eb80cbdc45381b1f15
Adrien Toubiana <adrientoub@hotmail.com>
2017-04-22 01:06:54 +0200

    Add ability to set a base directory

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,29 +1,36 @@
 class IndexController < ApplicationController
+  BASE_DIRECTORY = ENV['BASE_DIRECTORY'] || '.'
+
   def index
-    @directory = Dir.entries('.')
+    @directory = Dir.entries(BASE_DIRECTORY)
     @path = ''
+    @absolute_path = BASE_DIRECTORY
   end
 
   def path
     check_path
-    if File.directory?(params[:path])
-      @directory = Dir.entries(params[:path])
+
+    if File.directory?(@absolute_path)
+      @directory = Dir.entries(@absolute_path)
       @path = "#{params[:path]}/"
       render :index
-    elsif File.size(params[:path]) > 250_000
-      send_file params[:path]
-    else
-      @file = File.read(params[:path])
-      render :file
+    elsif File.file?(@absolute_path)
+      if File.size(@absolute_path) > 250_000
+        send_file @absolute_path
+      else
+        @file = File.read(@absolute_path)
+        render :file
+      end
     end
   end
 
   def delete
     check_path
-    if File.directory?(params[:path])
-      FileUtils.rm_rf(params[:path])
+
+    if File.directory?(@absolute_path)
+      FileUtils.rm_rf(@absolute_path)
     else
-      FileUtils.rm(params[:path])
+      FileUtils.rm(@absolute_path)
     end
     head 204
   end
@@ -31,9 +38,11 @@ class IndexController < ApplicationController
   private
 
   def check_path
-    current_directory = File.expand_path('.')
-    path = File.expand_path(params[:path])
-    unless path.starts_with?(current_directory)
+    current_directory = File.expand_path(BASE_DIRECTORY)
+    @absolute_path = File.expand_path(params[:path], BASE_DIRECTORY)
+    raise ActionController::RoutingError, 'Not Found' unless File.exists?(@absolute_path)
+
+    unless @absolute_path.starts_with?(current_directory)
       raise ArgumentError, 'Should not be parent of root'
     end
   end
