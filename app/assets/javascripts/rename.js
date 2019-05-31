b159095c1594f6ac0c4073e56da674423df30b10
Adrien Toubiana <adrientoub@hotmail.com>
2019-05-31 21:32:30 +0200

    Fix rename from subdirectory

--- app/assets/javascripts/rename.js
+++ app/assets/javascripts/rename.js
@@ -2,7 +2,7 @@ function rename(name) {
   var new_name = prompt("Enter the new name for this element.", name);
   if (new_name != null) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("PUT", name, false);
+    xmlHttp.open("PUT", '/' + name, false);
     var data = new FormData();
     data.append("new_name", new_name);
     var csrf_token = document
